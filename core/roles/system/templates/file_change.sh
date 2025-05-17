5a1288e706842238060d92403473cf3856f15aa0
ldelatte <laurent.delatte@laposte.net>
2025-05-17 22:03:10 +0200

    fix file_change.sh

--- core/roles/system/templates/file_change.sh
+++ core/roles/system/templates/file_change.sh
@@ -10,8 +10,9 @@ MSOD="{{ data_dir }}/msOneDrive"
 
 inotifywait -mrq -e create -e moved_to --format "%w%f" --exclude lost+found $TELE $MSOD | while read FILENAME
 do
-  DIR=$(dirname $FILENAME)
-  FIC=$(basename $FILENAME)
+  DIR=$(dirname "$FILENAME")
+  FIC=$(basename "$FILENAME")
+  echo $FIC | grep -q -e ".tmp$" -e "^.~lock" && continue
   echo ""
   ls -lZ "$FILENAME"
   #
@@ -19,19 +20,19 @@ do
     echo Télé/...:
 	SDIR=$(echo $DIR | sed 's/\(.*Téléchargements.[^/]*\).*/\1/')
     echo réf $SDIR :
-	/usr/bin/chown --reference=$SDIR $FILENAME
-	/usr/bin/chcon --reference=$SDIR $FILENAME
-    /usr/bin/chmod g+rw $FILENAME
+	/usr/bin/chown --reference=$SDIR "$FILENAME"
+	/usr/bin/chcon --reference=$SDIR "$FILENAME"
+    /usr/bin/chmod g+rw "$FILENAME"
   elif echo $DIR | grep -q "Téléchargements" ;then
     echo réf $DIR :
-	/usr/bin/chown --reference=$DIR $FILENAME
-	/usr/bin/chcon --reference=$DIR $FILENAME
-    /usr/bin/chmod g+rw $FILENAME
+	/usr/bin/chown --reference=$DIR "$FILENAME"
+	/usr/bin/chcon --reference=$DIR "$FILENAME"
+    /usr/bin/chmod g+rw "$FILENAME"
   else # msod
     echo réf $DIR
-	/usr/bin/chown 131099:{{ ansible_user }} $FILENAME
-	/usr/bin/chcon --reference=$DIR $FILENAME
-    /usr/bin/chmod o-rwx $FILENAME
+	/usr/bin/chown 131099:{{ ansible_user }} "$FILENAME"
+	/usr/bin/chcon --reference=$DIR "$FILENAME"
+    /usr/bin/chmod o-rwx "$FILENAME"
   fi
   #
   ls -lZ "$FILENAME"
